d96bb6db855e561f13d7e086323ad7ff670f14d1
CiocBiancaIoana <57239871+CiocBiancaIoana@users.noreply.github.com>
2019-11-01 20:03:24 +0200

    Add files via upload

---
+++ E3_functie.m
@@ -0,0 +1,12 @@
+function E3_functie(a) %functia 'operatii complexe' 
+    
+  mean (real(a)) % se face media partilor reale ale matricei a
+  n=length(a);
+  for i=1:1:n % for de la 1 la n, i = i+1
+      b(i) = a(i)^2; %vectorul patratelor elem lui a
+  end
+  b(i) %se afiseaza matricea b
+  (a)*(a.') % a inmultit cu a transpus
+  
+  end 
+  
